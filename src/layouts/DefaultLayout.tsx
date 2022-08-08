0ad2c92a69589c3320c02927fa3057cda186687f
renglan1 <rob.eng@unb.ca>
2022-08-08 15:05:23 -0300

    Adjusted layout margins

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Wallpaper from '../components/Wallpaper';
 import Header from '../components/Header';
 import PropTypes from 'prop-types'
+import '../components/styles/default-layout.scss';
 
 type DefaultLayoutProps = {
   expanded: boolean,
@@ -10,16 +11,19 @@ type DefaultLayoutProps = {
 
 const DefaultLayout = ({ expanded, children }: DefaultLayoutProps) => {
   return (
-    <>
+    <div className="default-layout">
       <Header expanded={expanded}/>
       <Wallpaper />
       <main>
         {children}
       </main>
-    </>
+    </div>
   )
 }
 
-DefaultLayout.propTypes = {}
+DefaultLayout.propTypes = {
+  expanded: PropTypes.bool,
+  children: PropTypes.node,
+}
 
 export default DefaultLayout
