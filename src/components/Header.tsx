03dc2893c7c004f72bf6818db608c096356ee7dc
renglan1 <rob.eng@unb.ca>
2022-08-08 12:58:02 -0300

    fixed issue with header text size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export const Header = ({ condensed }: HeaderProps) => {
   return (
     <div className='header'>
       <img src={require('../assets/images/icon.svg').default} alt='' />
-      <h1>
+      <h1 className='title'>
         <SpacedText text='soularium' />
       </h1>
     </div>
