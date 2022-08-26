ee525641ea0ac74f9c5e9423aa75548770f5fedc
renglan1 <rob.eng@unb.ca>
2022-08-26 15:02:36 -0300

    added styled components typedefs

--- src/components/Wallpaper.tsx
+++ src/components/Wallpaper.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import styled from 'styled-components'
 import './styles/wallpaper.scss';
 
-const Wallpaper = () => {
-  return (
-    <div className='wallpaper'>
-        <img src={require('../assets/images/wallpaper.jpg')} alt='' />
-    </div>
-  );
-};
+interface WallpaperProps {
+    name: string;
+}
 
-export default Wallpaper;
+const WallpaperFrame = styled.div.attrs(props => ({className: 'wallpaper',}))`
+    position: fixed;
+    top: 0;
+    z-index: -1000;
+    overflow: hidden; 
+    width: 100%;
+    height: 100%;
+
+    > img {
+        object-fit: cover;
+        width: 100%;
+        height: 100%;
+    }
+`
+
+export default function Wallpaper({name}: WallpaperProps){
+    return(
+        <WallpaperFrame>
+            <img src={require(`@/assets/images/${name}`)} alt="wallpaper" />
+        </WallpaperFrame>
+    );
+}
