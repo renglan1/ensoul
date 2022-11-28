0118c5a033a44f011db5e135c42e4610e9d4a09d
Rob England <rob.eng@unb.ca>
2022-11-28 12:31:57 -0400

    fable cards moved to external json file

--- src/App.tsx
+++ src/App.tsx
@@ -5,46 +5,15 @@ import "assets/global.scss";
 import { fable } from "components/FragmentCards";
 
 function App() {
-  const fables = [
-    {
-      id: 1,
-      name: "Isha",
-      title: "the Revenant",
-    },
-    {
-      id: 2,
-      name: "Lydia",
-      title: "the Inquisitor",
-    },
-    {
-      id: 3,
-      name: "Astel",
-      title: "the Insatiable",
-    },
-    {
-      id: 4,
-      name: "Malakath",
-      title: "the Dread",
-    },
-    {
-      id: 5,
-      name: "Veer",
-      title: "the Hunter",
-    },
-    {
-      id: 6,
-      name: "Phoci",
-      title: "the Mage of the Golden Spire",
-    },
-  ];
+    const fables = require("data/fable_cards.json");
 
-  return (
-    <div className="App">
-      <DefaultLayout>
-        <FragmentCards fables={fables as Array<fable>} />
-      </DefaultLayout>
-    </div>
-  );
+    return (
+        <div className="App">
+            <DefaultLayout>
+                <FragmentCards fables={fables as Array<fable>} />
+            </DefaultLayout>
+        </div>
+    );
 }
 
 export default App;
